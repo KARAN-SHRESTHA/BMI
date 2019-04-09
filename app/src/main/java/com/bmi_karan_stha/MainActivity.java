d3da074f5be83bc31f442af0ce4b96a8c004162f
KARAN SHRESTHA <karan_shrestah1@outlook.com>
2019-04-09 18:48:27 +0545

    Creating Calculate java class

--- app/src/main/java/com/bmi_karan_stha/MainActivity.java
+++ app/src/main/java/com/bmi_karan_stha/MainActivity.java
@@ -2,12 +2,43 @@ package com.bmi_karan_stha;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private EditText etHeight, etWeight;
+    private Button btnCalculate;
+    private TextView tvBMI;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        etHeight = findViewById(R.id.etHeight);
+        etWeight = findViewById(R.id.etWeight);
+        btnCalculate = findViewById(R.id.btnCalculate);
+        tvBMI = findViewById(R.id.tvBMI);
+
+        btnCalculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                float Height = Integer.parseInt(etHeight.getText().toString());
+                float Weight = Integer.parseInt(etWeight.getText().toString());
+
+                Calculate c = new Calculate();
+
+
+            }
+        });
+
+
     }
 }
